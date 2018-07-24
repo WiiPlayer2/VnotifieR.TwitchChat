59c2489f9d140db68bfbbf48df5552d7386b5282
Waldemar Tomme <waldetomme@gmail.com>
2018-07-24 18:10:34 +0200

    Added basic logic (pls no judge, it's just temporary)

---
+++ VnotifieR.TwitchChat/Notification.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VnotifieR.TwitchChat
+{
+    class Notification
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+    }
+}
--- VnotifieR.TwitchChat/Program.cs
+++ VnotifieR.TwitchChat/Program.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
 
 namespace VnotifieR.TwitchChat
 {
     class Program
     {
+        private static Ini ini;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ini = new Ini("./config.ini");
+
+            var creds = new ConnectionCredentials(ini.GetValue("username"), ini.GetValue("token"));
+            var client = new TwitchClient();
+            client.Initialize(creds, ini.GetValue("username"));
+
+            client.OnJoinedChannel += Client_OnJoinedChannel;
+            client.OnLeftChannel += Client_OnLeftChannel;
+            client.OnMessageReceived += Client_OnMessageReceived;
+
+            client.Connect();
+            Thread.Sleep(-1);
+        }
+
+        private static void Client_OnLeftChannel(object sender, TwitchLib.Client.Events.OnLeftChannelArgs e)
+        {
+            SendMessage("SYSTEM", "left channel");
+        }
+
+        private static void Client_OnMessageReceived(object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
+        {
+            SendMessage(e.ChatMessage.Username, e.ChatMessage.Message);
+        }
+
+        private static void Client_OnJoinedChannel(object sender, TwitchLib.Client.Events.OnJoinedChannelArgs e)
+        {
+            SendMessage("SYSTEM", "joined channel");
+        }
+
+        private static void SendMessage(string title, string content)
+        {
+            var request = WebRequest.CreateHttp(ini.GetValue("url"));
+            request.Method = "POST";
+            using (var writer = new StreamWriter(request.GetRequestStream()))
+            {
+                writer.Write(JsonConvert.SerializeObject(new Notification
+                {
+                    Title = title,
+                    Content = content,
+                }));
+                writer.Flush();
+            }
+            try
+            {
+                request.GetResponse();
+            }
+            catch { }
         }
     }
 }
